c0677ced0812ed9281b2f5980dff9f6ff587fcae
tancredesimonin <tancredesimonin@proton.me>
2024-09-16 16:42:48 +0200

    🤔 - tsconfig stuff we dont like

---
+++ src/typewriter-tools.d.ts
@@ -0,0 +1,7 @@
+declare module "typewriter-tools/server" {
+  export * from "typewriter-tools/dist/server";
+}
+
+declare module "typewriter-tools/client" {
+  export * from "typewriter-tools/dist/client";
+}
