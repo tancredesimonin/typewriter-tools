fc561f818c20c051f8367b5e26e95677df221b6a
tancredesimonin <tancredesimonin@proton.me>
2024-09-20 14:37:24 +0200

    🤔 - so boring compiler config

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,10 +1,10 @@
 /**
  * types
  */
-export { TypewriterConfig } from "./config/typewriter.config.js";
-export { Website } from "./types/website.js";
-export { HomePage } from "./types/pages.js";
-export { Article, PageArticlesList } from "./types/articles.js";
-export { Tag, PageTagsList } from "./types/tags.js";
-export { Category, PageCategoriesList } from "./types/categories.js";
-export { Serie, PageSeriesList } from "./types/series.js";
+export { TypewriterConfig } from "./config/typewriter.config";
+export { Website } from "./types/website";
+export { HomePage } from "./types/pages";
+export { Article, PageArticlesList } from "./types/articles";
+export { Tag, PageTagsList } from "./types/tags";
+export { Category, PageCategoriesList } from "./types/categories";
+export { Serie, PageSeriesList } from "./types/series";
--- src/typewriter-tools.d.ts
+++ src/typewriter-tools.d.ts
@@ -1,15 +1,15 @@
 declare module "typewriter-tools/server" {
-  export * from "./dist/server/index.js";
+  export * from "./server/index.js";
 }
 
 declare module "typewriter-tools/client" {
-  export * from "./dist/client/index.js";
+  export * from "./client/index.js";
 }
 
 declare module "typewriter-tools/manager" {
-  export * from "./dist/manager/index.js";
+  export * from "./manager/index.js";
 }
 
 declare module "typewriter-tools/shared" {
-  export * from "./dist/shared/index.js";
+  export * from "./shared/index.js";
 }
