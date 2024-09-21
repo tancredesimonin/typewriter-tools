94c8975343f7d124b4929da0fb0980a54d3ecbe2
tancredesimonin <tancredesimonin@proton.me>
2024-09-21 15:59:21 +0200

    📆 - add date formatter

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,10 +1,19 @@
 /**
- * types
+ * Config
  */
 export { TypewriterConfig } from "./config/typewriter.config.js";
+
+/**
+ * Types
+ */
 export { Website } from "./types/website.js";
 export { HomePage } from "./types/pages.js";
 export { Article, PageArticlesList } from "./types/articles.js";
 export { Tag, PageTagsList } from "./types/tags.js";
 export { Category, PageCategoriesList } from "./types/categories.js";
 export { Serie, PageSeriesList } from "./types/series.js";
+
+/**
+ * Utils
+ */
+export { formatDate } from "./utils/date.utils.js";
