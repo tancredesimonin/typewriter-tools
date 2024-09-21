919249f6462a8f297a92dfc5c5734ea693e0859d
tancredesimonin <tancredesimonin@proton.me>
2024-09-21 16:03:54 +0200

    🕷️ - fix formatDate

--- src/shared/utils/date.utils.ts
+++ src/shared/utils/date.utils.ts
@@ -1,3 +1,13 @@
-export function formatDate(date: string) {
-  return new Date(date).toISOString().split("T")[0];
+/**
+ * Formats a date string to the format YYYY-MM-DD.
+ * If the date is invalid, it returns the current date.
+ * @param date - The date string to format.
+ * @returns The formatted date string.
+ */
+export function formatDate(date: string): string {
+  const dateObj = new Date(date);
+  if (isNaN(dateObj.getTime())) {
+    return new Date().toISOString().split("T")[0]!;
+  }
+  return dateObj.toISOString().split("T")[0]!;
 }
