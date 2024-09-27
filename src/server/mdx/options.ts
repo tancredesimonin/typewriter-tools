df54de60ca8185532209d38882ab78a3549511ce
tancredesimonin <tancredesimonin@proton.me>
2024-09-27 12:41:00 +0200

    🌈 - add default colors hex

--- src/server/mdx/options.ts
+++ src/server/mdx/options.ts
@@ -1,11 +1,18 @@
-import { Color, validColors } from "../../shared/utils/colors.utils.js";
+import {
+  Color,
+  defaultColors,
+  validColors,
+} from "../../shared/utils/colors.utils.js";
 import { iconKeys, IconName } from "../../shared/utils/icons.utils.js";
 
 export class MDXOptionRepository {
   public readonly colors: Color[];
+  public readonly defaultColors: { name: Color; hex: string }[];
   public readonly icons: IconName[];
   constructor() {
     this.colors = [...validColors];
+    this.defaultColors = defaultColors;
+
     this.icons = [...iconKeys];
   }
 }
--- src/shared/utils/colors.utils.ts
+++ src/shared/utils/colors.utils.ts
@@ -21,6 +21,23 @@ export const validColors = [
 ] as const;
 
 export type Color = (typeof validColors)[number];
+export const defaultColors: { name: Color; hex: string }[] = [
+  { name: "red", hex: "#ef4444" },
+  { name: "orange", hex: "#f97316" },
+  { name: "amber", hex: "#eab308" },
+  { name: "yellow", hex: "#eab308" },
+  { name: "lime", hex: "#84cc16" },
+  { name: "green", hex: "#10b981" },
+  { name: "teal", hex: "#06b6d4" },
+  { name: "sky", hex: "#0ea5e9" },
+  { name: "blue", hex: "#3b82f6" },
+  { name: "indigo", hex: "#6366f1" },
+  { name: "violet", hex: "#8b5cf6" },
+  { name: "purple", hex: "#a855f7" },
+  { name: "fuchsia", hex: "#ec4899" },
+  { name: "pink", hex: "#f472b6" },
+  { name: "rose", hex: "#f43f5e" },
+];
 
 export function isValidColor(color: string): color is Color {
   return (validColors as readonly string[]).includes(color);
